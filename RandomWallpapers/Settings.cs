8e36f74a851f2e1719d52653439de36b623a0266
Rahiyan Safin <70625091+rahiyansafz@users.noreply.github.com>
2023-03-10 01:50:08 +0600

    Update Settings.cs

--- RandomWallpapers/Settings.cs
+++ RandomWallpapers/Settings.cs
@@ -11,6 +11,6 @@ public static class Settings
     public static readonly string QueryString = "Concert, Party, Clubbing, Pretty Girls, Fashion, Models, Lifestyle, Passion, Career, Programming, Coding, City, Couple, Music, Film, Athletics, Interior Design, Living Room, Bedroom, Office, City, Night City, Arts & Culture, People, Food & Drink";
 
     public static readonly string BaseURL = "https://api.unsplash.com/";
-    public static readonly string AccessKey = "IYhP0XBPgS8ebbHpmyVeYZ7aLqsLPfXG-wmWVbcHHWw";
+    public static readonly string AccessKey = "YOUR_API_KEY";
     public static readonly string Secretkey = string.Empty;
 }
